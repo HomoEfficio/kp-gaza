6e98f8f152d43acf4888d98734b0218ccf66940c
Homo Efficio <homo.efficio@gmail.com>
2020-06-26 20:14:03 +0900

    대화방 입장 컨트롤러 구현 - 테스트 통과

--- src/main/java/io/homo_efficio/kpgaza/mvc/controller/ChatRoomController.java
+++ src/main/java/io/homo_efficio/kpgaza/mvc/controller/ChatRoomController.java
@@ -2,13 +2,13 @@ package io.homo_efficio.kpgaza.mvc.controller;
 
 import io.homo_efficio.kpgaza.mvc.dto.ChatRoomIn;
 import io.homo_efficio.kpgaza.mvc.dto.ChatRoomOut;
+import io.homo_efficio.kpgaza.mvc.dto.ChatUserOut;
 import io.homo_efficio.kpgaza.mvc.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
  * @author homo.efficio@gmail.com
@@ -24,5 +24,10 @@ public class ChatRoomController {
     @PostMapping
     public ResponseEntity<ChatRoomOut> create(@RequestBody ChatRoomIn chatRoomIn) {
         return ResponseEntity.ok(chatRoomService.create(chatRoomIn));
+    }
+
+    @PostMapping("/{chatRoomId}/chatters/{chatterId}")
+    public ResponseEntity<ChatUserOut> createChatUser(@PathVariable UUID chatRoomId, @PathVariable Long chatterId) {
+        return ResponseEntity.ok(chatRoomService.createChatUser(chatRoomId, chatterId));
     }
 }
