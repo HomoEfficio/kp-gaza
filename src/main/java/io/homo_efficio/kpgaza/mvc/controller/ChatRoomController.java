a7e6a9188d6528ec3a7cfc0327c436d4c252a1d5
Homo Efficio <homo.efficio@gmail.com>
2020-06-26 12:46:02 +0900

    ChatRoomController 생성 구현 - 테스트 통과

--- src/main/java/io/homo_efficio/kpgaza/mvc/controller/ChatRoomController.java
+++ src/main/java/io/homo_efficio/kpgaza/mvc/controller/ChatRoomController.java
@@ -1,8 +1,28 @@
 package io.homo_efficio.kpgaza.mvc.controller;
 
+import io.homo_efficio.kpgaza.mvc.dto.ChatRoomIn;
+import io.homo_efficio.kpgaza.mvc.dto.ChatRoomOut;
+import io.homo_efficio.kpgaza.mvc.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author homo.efficio@gmail.com
  * created on 2020-06-26
  */
+@RequestMapping("/chat-rooms")
+@RestController
+@RequiredArgsConstructor
 public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+
+    @PostMapping
+    public ResponseEntity<ChatRoomOut> create(@RequestBody ChatRoomIn chatRoomIn) {
+        return ResponseEntity.ok(chatRoomService.create(chatRoomIn));
+    }
 }
