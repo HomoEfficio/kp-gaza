d5efa74b148af4366aea74014e0588dadd1491c5
Homo Efficio <homo.efficio@gmail.com>
2020-06-26 10:27:55 +0900

    JPA 기본 설정

---
+++ src/main/java/io/homo_efficio/kpgaza/mvc/_config/JpaConfig.java
@@ -0,0 +1,14 @@
+package io.homo_efficio.kpgaza.mvc._config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2020-06-26
+ */
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
+
