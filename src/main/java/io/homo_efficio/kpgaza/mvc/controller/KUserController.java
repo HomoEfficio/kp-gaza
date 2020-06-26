ee7bede735f9f8f02680934ff0e36478611c3171
Homo Efficio <homo.efficio@gmail.com>
2020-06-26 11:38:56 +0900

    KUserControllerTest 추가

---
+++ src/main/java/io/homo_efficio/kpgaza/mvc/controller/KUserController.java
@@ -0,0 +1,15 @@
+package io.homo_efficio.kpgaza.mvc.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2020-06-26
+ */
+@RequestMapping("/kusers")
+@RestController
+@RequiredArgsConstructor
+public class KUserController {
+}
---
+++ src/main/java/io/homo_efficio/kpgaza/mvc/dto/KUserIn.java
@@ -0,0 +1,26 @@
+package io.homo_efficio.kpgaza.mvc.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2020-06-26
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class KUserIn {
+
+    private Long id;
+
+    @NotEmpty
+    @Min(value = 2, message = "사용자 이름은 2자 이상이어야 합니다.")
+    @Max(value = 20, message = "사용자 이름은 10자 이내여야 합니다.")
+    private String name;
+}
