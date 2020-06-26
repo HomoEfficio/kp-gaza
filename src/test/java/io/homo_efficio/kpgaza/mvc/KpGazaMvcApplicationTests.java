65315f89c5b958074edf123b926da0aa7c5b29c6
Homo Efficio <homo.efficio@gmail.com>
2020-06-26 10:18:38 +0900

    Initial Commit

---
+++ src/test/java/io/homo_efficio/kpgaza/mvc/KpGazaMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.kpgaza.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpGazaMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
