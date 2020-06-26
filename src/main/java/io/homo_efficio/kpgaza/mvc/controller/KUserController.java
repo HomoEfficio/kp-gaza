d335409f9c9c2b006c79e60bbc070f79819fb72d
Homo Efficio <homo.efficio@gmail.com>
2020-06-26 11:52:05 +0900

    KUserController 생성 구현 - 테스트 통과

--- src/main/java/io/homo_efficio/kpgaza/mvc/controller/KUserController.java
+++ src/main/java/io/homo_efficio/kpgaza/mvc/controller/KUserController.java
@@ -1,6 +1,12 @@
 package io.homo_efficio.kpgaza.mvc.controller;
 
+import io.homo_efficio.kpgaza.mvc.dto.KUserIn;
+import io.homo_efficio.kpgaza.mvc.dto.KUserOut;
+import io.homo_efficio.kpgaza.mvc.service.KUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class KUserController {
+
+    private final KUserService kUserService;
+
+    @PostMapping
+    public ResponseEntity<KUserOut> create(@RequestBody KUserIn kUserIn) {
+        return ResponseEntity.ok(kUserService.create(kUserIn));
+    }
 }
