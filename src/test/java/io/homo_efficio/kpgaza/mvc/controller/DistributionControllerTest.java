6abe0237dfb8e06c9e784c5513808569933fcfb6
Homo Efficio <homo.efficio@gmail.com>
2020-06-27 12:53:20 +0900

    본인만 뿌리기 정보 조회 가능 테스트 추가

--- src/test/java/io/homo_efficio/kpgaza/mvc/controller/DistributionControllerTest.java
+++ src/test/java/io/homo_efficio/kpgaza/mvc/controller/DistributionControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.util.NestedServletException;
 
 import java.util.Collections;
 import java.util.List;
@@ -27,6 +28,7 @@ import java.util.UUID;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -145,5 +147,25 @@ class DistributionControllerTest {
                 .andExpect(jsonPath("receipts").isArray())
                 .andExpect(jsonPath("receipts").isEmpty())
         ;
+    }
+
+    @ParameterizedTest(name = "본인이 아니면 뿌리기 정보 조회는 실패한다.")
+    @MethodSource("unauthorizedRequesters")
+    @Sql(scripts = "classpath:init-receipts.sql")
+    void onlyDistributorCanSeeHisDistribution(String token, Long requesterId) {
+        assertThrows(NestedServletException.class, () ->
+                mvc.perform(get("/distributions?token=" + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("X-USER-ID", requesterId))
+                        .andExpect(status().isOk()));
+    }
+
+    private static Stream<Arguments> unauthorizedRequesters() {
+        return Stream.of(
+                Arguments.of("a11", 2L),
+                Arguments.of("a21", 3L),
+                Arguments.of("c41", 9L)
+        );
     }
 }
