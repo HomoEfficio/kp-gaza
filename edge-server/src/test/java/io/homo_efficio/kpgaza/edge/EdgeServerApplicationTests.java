327173c126e3f12b98a626b88d83da6d095ded2f
Homo Efficio <homo.efficio@gmail.com>
2020-06-27 16:04:40 +0900

    Edge Server 추가

---
+++ edge-server/src/test/java/io/homo_efficio/kpgaza/edge/EdgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.kpgaza.edge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
